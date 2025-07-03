9a5b0fe8b4db4521920a4c53107d4fe6c1ada857
NatalyPetrova <zimnata96@mail.ru>
2025-07-03 13:53:25 +0900

    Add quiz with 3 questions in Korean

--- src/App.js
+++ src/App.js
@@ -1,23 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.scss";
+
+const questions = [
+  {
+    title: "한국의 수도는 어디입니까?",
+    variants: ["서울", "부산", "인천"],
+    correct: 0,
+  },
+  {
+    title: "한글을 만든 왕은 누구입니까? ",
+    variants: ["세종대왕", "이순신", "광개토대왕"],
+    correct: 0,
+  },
+  {
+    title: "다음 중 한국 음식이 아닌 것은?",
+    variants: ["김치", "불고기", "스시"],
+    correct: 2,
+  },
+];
+
+function Result({ correct }) {
+  return (
+    <div className="result">
+      <img src="https://cdn-icons-png.flaticon.com/512/2278/2278992.png" />
+      <h2>
+        Вы отгадали {correct} ответа из {questions.length}
+      </h2>
+      <a href="/">
+        <button>Попробовать снова</button>
+      </a>
+    </div>
+  );
+}
+
+function Game({ step, question, onClickVariant }) {
+  const percentage = Math.round((step / questions.length) * 100);
+
+  console.log(percentage);
+
+  return (
+    <>
+      <div className="progress">
+        <div
+          style={{ width: `${percentage}%` }}
+          className="progress__inner"
+        ></div>
+      </div>
+      <h1>{question.title}</h1>
+      <ul>
+        {question.variants.map((text, index) => (
+          <li onClick={() => onClickVariant(index)} key={text}>
+            {text}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
 
 function App() {
+  const [step, setStep] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const question = questions[step];
+
+  const onClickVariant = (index) => {
+    console.log(step, index);
+    setStep(step + 1);
+
+    if (index === question.correct) {
+      setCorrect(correct + 1);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {step !== questions.length ? (
+        <Game step={step} question={question} onClickVariant={onClickVariant} />
+      ) : (
+        <Result correct={correct} />
+      )}
     </div>
   );
 }
